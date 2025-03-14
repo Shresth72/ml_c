4e81de4938b54e343433a0edb757149fc012e675
Shresth72 <97455610+Shresth72@users.noreply.github.com>
2025-03-14 09:32:26 +0000

    adding bias

--- main.c
+++ main.c
@@ -7,11 +7,11 @@ float train[][2] = {{0, 0}, {1, 2}, {2, 4}, {3, 6}, {4, 8}};
 
 float rand_float(void) { return (float)rand() / (float)RAND_MAX; }
 
-float cost(float w) {
+float cost_function(float w, float b) {
   float result = 0.0f;
   for (size_t i = 0; i < train_count; ++i) {
     float x = train[i][0];
-    float y = x * w;
+    float y = x * w + b;
     float d = y - train[i][1];
     result += d * d;
   }
@@ -24,7 +24,24 @@ int main() {
   srand(69);
 
   float w = rand_float() * 10.0f;
-  printf("%f\n", cost(w));
+  float b = rand_float() * 2.0f;
+
+  float eps = 1e-3;
+  float lrate = 1e-3;
+
+  printf("%f\n", cost_function(w, b));
+
+  for (size_t i = 0; i < 350; ++i) {
+    float cost = cost_function(w, b);
+    float dweight = (cost_function(w + eps, b) - cost) / eps;
+    float dbias = (cost_function(w, b + eps) - cost) / eps;
+    w -= lrate * dweight;
+    b -= lrate * dbias;
+    printf("cost = %f, w = %f, b = %f\n", cost_function(w, b), w, b);
+  }
+
+  printf("--------------------\n");
+  printf("w = %f, b = %f\n", w, b);
 
   return 0;
 }
